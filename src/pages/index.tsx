7b033fb2fe8a598cba7551168c4f627a01c7b1a0
marie <marielichionis@gmail.com>
2023-02-05 00:21:53 -0500

    version 6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import CourseAssignments from "../components/CourseAssignments";
 import CourseList from "../components/CourseList";
 
 const Home: NextPage = () => {
